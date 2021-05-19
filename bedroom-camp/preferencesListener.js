edc7573c23e4fc38257ca7f6e37e6314bbae5487
Alex Kupin <interstates21@gmail.com>
2021-05-19 20:14:40 +0300

    added modal

---
+++ bedroom-camp/preferencesListener.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // const preferencesButton = document.getElementById('preferences-button');
+
+    // console.log('preferencesButton', preferencesButton)
+
+
+});
