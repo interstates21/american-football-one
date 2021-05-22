26ff684589faec47d180dfb8723650bafc2431d7
Maria <maria-himoch@yandex.ru>
2021-05-22 13:23:59 +0200

    maria-test-js

---
+++ js-practice/maria-practice/index.js
@@ -0,0 +1,43 @@
+window.addEventListener("load", () => {
+   
+    const baloonRef = document.getElementById('baloon');
+
+    const blackState = {
+        // id: "black",
+        backgroundColor: baloonRef.style.backgroundColor,
+        width: parseInt(baloonRef.style.width),
+   
+    }
+    const bluekState = {
+
+        backgroundColor: 'royalblue',
+        width: 300,
+    }
+
+    let status = blackState;
+
+
+
+    baloonRef.addEventListener('click', (event) => {
+        if (status !== blackState){
+            status = blackState;
+      
+    } else {
+        status = bluekState;
+    }
+
+    baloonRef.style.backgroundColor = status.backgroundColor;
+    baloonRef.style.width = status.width + "px";
+
+
+        
+
+
+
+        // if (ageNum >= 20) {
+        //     hiddenCheckboxRef.style.display = 'block';
+        // } else {
+        //     hiddenCheckboxRef.style.display = 'none';
+        // }
+    })
+})
