ca0ec9f99aa513568d81b8ad2eb35986da01674a
Maria <maria-himoch@yandex.ru>
2021-05-23 11:28:16 +0200

    maria-JS

--- js-practice/maria-practice/test/test.js
+++ js-practice/maria-practice/test/test.js
@@ -1,11 +1,55 @@
 window.addEventListener("load", () => {
    
-    const orange = document.getElementById('orange');
-    let color = "white";
+    const orangeColor = document.getElementById('orange');
+    const grayColor = document.getElementById('gray');
+    const brownColor = document.getElementById('brown');
+    const yellowColor = document.getElementById('yellow');
 
-     orange.addEventListener('click', (event) => {
-        color = "orange";
-        orange.style.border = "rgb(255, 115, 0) solid 5px;" ;
-        console.log(color);
-})
-})
+   let orange = false;
+   let grey = false;
+   let brown = false;
+   let yellow = false;
+
+
+
+   orangeColor.addEventListener('click', (event) => {
+    orange = true;
+    grey = false;
+    brown = false;
+    yellow = false;
+
+    console.log(orange, grey, brown, yellow);
+    })
+
+
+    grayColor.addEventListener('click', (event) => {
+        orange = false;
+        grey = true;
+        brown = false;
+        yellow = false;
+        })
+
+    brownColor.addEventListener('click', (event) => {
+            orange = false;
+            grey = false;
+            brown = true;
+            yellow = false;
+            }) 
+
+    yellowColor.addEventListener('click', (event) => {
+                orange = false;
+                grey = false;
+                brown = false;
+                yellow = true;
+                })
+    
+    
+   
+
+
+
+
+
+
+    })
+
