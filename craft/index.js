1bad171342492fde0edbe314021a10c1b7cc6402
Alex Kupin <interstates21@gmail.com>
2021-05-29 13:42:15 +0300

    Recipes started

--- craft/index.js
+++ craft/index.js
@@ -1,4 +1,4 @@
-const CRAFT_DATA = [
+const MATERIALS_DATA = [
     {
         name: 'Wood',
         amount: 2,
@@ -10,32 +10,53 @@ const CRAFT_DATA = [
         iconPath: './pics/iron.svg'
     },
 ]
+
+const RECIPES = [
+    {
+        name: 'Sabre',
+        iconPath: './pics/sparkling-sabre.svg',
+        iron: 1,
+    },
+    {
+        name: 'Cosmic Egg',
+        iconPath: './pics/cosmic-egg.svg',
+        iron: 90,
+    },
+]
+
 const allMaterials = document.querySelector('#all-materials');
 
 let index = 0;
 
-while (index < CRAFT_DATA.length){
+while (index < MATERIALS_DATA.length){
     const createdMaterialDiv = document.createElement('div');
     createdMaterialDiv.className = 'material';
 
     allMaterials.appendChild(createdMaterialDiv);
 
     const image = document.createElement('img');
-    image.src =CRAFT_DATA[index].iconPath;
+    image.src =MATERIALS_DATA[index].iconPath;
     createdMaterialDiv.appendChild(image);
 
     // String literal allows to pass a JS string variable into a string
     // Injecting html into a created element
     // createdMaterialDiv.innerHTML = `
-    // <img src="${CRAFT_DATA[index].iconPath}"> </img>
-    // <p class="material-label">${CRAFT_DATA[index].name}</p>
+    // <img src="${MATERIALS_DATA[index].iconPath}"> </img>
+    // <p class="material-label">${MATERIALS_DATA[index].name}</p>
     // `
 
     // Manually add elements to a created element 
     const nameOfMaterial = document.createElement('p');
-    nameOfMaterial.innerHTML =CRAFT_DATA[index].name;
+    nameOfMaterial.innerHTML =MATERIALS_DATA[index].name;
     nameOfMaterial.className = 'material-label';    
     createdMaterialDiv.appendChild(nameOfMaterial);
 
     index++
 }
+
+{/* <div id="craft-container">
+<div class="craft-element craft-available">
+    <img src="./pics/sparkling-sabre.svg" />
+    <p class="craft-label">Wood: 4, Iron: 2</p>
+</div>
+</div> */}
