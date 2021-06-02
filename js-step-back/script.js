654e4e8e1131ce85e428ac1c4d5709644d4887f9
Jon Fahlberg <jon.fahlberg@rccvast.se>
2021-06-02 19:34:38 +0200

    Some practice

--- js-step-back/script.js
+++ js-step-back/script.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const button1Ref = document.getElementById('button1')
+
+
+
+
+const person = {
+    firstName: 'Jon',
+    lastName: 'Fahlberg',
+    age: 100,
+
+}
+const firstname = person['firstName']
+const lastName = person.lastName
+console.log("person=", person)
