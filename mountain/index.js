c5431c8ab5a5902f5fa49543af0ddfddb7b80d45
Alex Kupin <interstates21@gmail.com>
2021-06-14 19:27:33 +0300

    created a new project

---
+++ mountain/index.js
@@ -0,0 +1,13 @@
+// const someElement = document.getElementById('some-id');
+
+// const newElement = document.createElement('div');
+// selectedParent.appendChild(newElement)
+
+// someElement.addEventListener('click', () => {
+//     do some stuff when this element has been clicked
+// })
+
+// window.setTimout(() => {
+//     do some stuff after one second
+
+// }, 1000)
