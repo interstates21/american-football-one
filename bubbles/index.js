c5001757775b1c2170be002842115686d4c24ddc
Alex Kupin <interstates21@gmail.com>
2021-06-28 21:11:37 +0300

    Formatting

--- bubbles/index.js
+++ bubbles/index.js
@@ -1,26 +1,24 @@
-const createDevilFunction =(x,y) => {
-
-const image = document.createElement("img")
-image.src = "./pics/devil.png"
-image.style.width = "10vw"
-image.style.height = "10vw"
-image.style.position = "absolute"
-image.style.left = `${x*10}vw`
-image.style.top = `${y*10}vw`
-document.body.appendChild(image)
+const createDevilFunction =(xIndex, yIndex) => {
+    const image = document.createElement("img")
+    image.src = "./pics/devil.png"
+    image.style.width = "50px"
+    image.style.height = "50px"
+    image.style.position = "absolute"
+    image.style.left = `${xIndex * 55}px`
+    image.style.top = `${yIndex * 55}px`
+    document.body.appendChild(image)
 }
-
 
 const createRow =(n) =>{
-    let i=0
-while (i<10) {
-createDevilFunction(i,n)
-i++
-}
+    let x = 0
+    while (x < 10) {
+        createDevilFunction(10 - x , n)
+        x++
+    }
 }
 
-let m=0
-while (m<10) {
-    createRow(m)
-    m++
+let y = 0
+while (y < 10) {
+    createRow(y)
+    y++
 }
