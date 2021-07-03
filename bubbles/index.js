00d24b96de6515dd4f127942ac71e018445953e4
Alex Kupin <interstates21@gmail.com>
2021-07-03 13:48:02 +0300

    Teris partial

--- bubbles/index.js
+++ bubbles/index.js
@@ -1,18 +1,30 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
 const createDevilFunction =(xIndex, yIndex) => {
-    const image = document.createElement("img")
-    image.src = "./pics/devil.png"
+    const image = document.createElement("div")
     image.style.width = "50px"
     image.style.height = "50px"
+
+    // rgb(255, 255, 255)
+    // image.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`
+    image.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`
+
+    window.setInterval(() => {
+        image.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`
+    }, 100)
     image.style.position = "absolute"
     image.style.left = `${xIndex * 55}px`
     image.style.top = `${yIndex * 55}px`
     document.body.appendChild(image)
 }
 
-const createRow =(n) =>{
+const createRow =(n) => {
     let x = 0
     while (x < 10) {
-        createDevilFunction(10 - x , n)
+        createDevilFunction(x , n)
         x++
     }
 }
