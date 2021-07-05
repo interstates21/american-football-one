5e76225a40ff913921e75e801807ad6b531012c4
Maria <maria-himoch@yandex.ru>
2021-07-05 20:07:04 +0200

    messange

---
+++ messenger/index.js
@@ -0,0 +1,36 @@
+//Send message
+//Delete message
+//Edit message
+//Display messages
+
+const sendMessage = (text) => {
+
+}
+
+const deliteMessage = (text) => {
+    alert (text) 
+}
+
+const editMessage = (text) => {
+
+}
+
+const displayMessages = (x) => {
+
+
+    console.log(x)
+}
+
+const main = () => {
+    const MESSAGES = [
+        'Hello',
+        'Bye',
+        'Cat'
+    ]
+
+    displayMessages(MESSAGES)
+    
+}
+
+main()
+deliteMessage("yt")
