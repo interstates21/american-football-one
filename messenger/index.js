d62e22488b6155e100b6090d6e0d73d16b8d5bec
Alex Kupin <interstates21@gmail.com>
2021-07-07 20:24:46 +0300

    Messages

--- messenger/index.js
+++ messenger/index.js
@@ -1,36 +1,72 @@
-//Send message
-//Delete message
-//Edit message
-//Display messages
-
-const sendMessage = (text) => {
-
-}
-
-const deliteMessage = (text) => {
-    alert (text) 
-}
-
-const editMessage = (text) => {
-
-}
-
-const displayMessages = (x) => {
-
-
-    console.log(x)
-}
 
 const main = () => {
-    const MESSAGES = [
-        'Hello',
-        'Bye',
-        'Cat'
-    ]
 
-    displayMessages(MESSAGES)
+    let nextMessageId = 0;
+    const MESSAGES = []
+
+    const sendMessage = (text) => {
+        MESSAGES.push({
+            id: nextMessageId,
+            text: text,
+        })
+        nextMessageId++;
+    }
+
+    const deleteMessage = (messageId) => {
+        const foundTargetIndex = MESSAGES.findIndex(currentElement => {
+            if (currentElement.id === messageId) {
+                return true;
+            }
+            return false
+        })
+
+
+        // Array.findIndex returns -1 in case we couldn't find the right object (element) 
+        if (foundTargetIndex !== -1) {
+            MESSAGES.splice(foundTargetIndex, 1);
+        }
+        else {
+            alert('Invalid ID!!!')
+        }
+    }
+
+
+    const displayMessages = () => {
+        for (let i = 0; i < MESSAGES.length; i++) {
+            createMessage(MESSAGES[i]);
+        }
+    }
+
+    const createMessage = (message) => {
+        const messageArea = document.getElementById('messages-area');
     
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('one-message');
+
+        if (message.id === 0) {
+            newMessage.classList.add('message-selected')
+        }
+    
+        newMessage.innerText = message.text;
+    
+        messageArea.appendChild(newMessage);
+    }
+
+
+    const editMessage = (messageId, newText) => {
+        console.log(`Edited ${messageId}: ${newText}`)
+    }
+    
+    
+    sendMessage('Hello');
+    sendMessage('Hello1');
+    sendMessage('Hello2');
+    sendMessage('Hello3');
+
+    displayMessages();
+
+
+
 }
 
 main()
-deliteMessage("yt")
