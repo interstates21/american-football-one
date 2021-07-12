2ecf326d278f03134b6b1cd3281b22f8fca56e86
Alex Kupin <interstates21@gmail.com>
2021-07-12 19:42:47 +0300

    random

---
+++ function3/index.js
@@ -0,0 +1,21 @@
+const getRandomInt = (max) => {
+    // Got from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    return Math.floor(Math.random() * max);
+}
+
+const A = []
+
+const B = []
+
+const C = []
+
+const N = []
+
+
+const generateRandomName = () => {
+
+}
+
+const randomName = generateRandomName();
+
+console.log(randomName);
