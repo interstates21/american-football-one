c67e49e1d377fcf06ef2398f83d2c4618d48d5fe
Dalitej <dalitej@gmail.com>
2021-07-12 19:23:46 +0200

    words

--- function3/index.js
+++ function3/index.js
@@ -3,19 +3,80 @@ const getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
 }
 
-const A = []
+const A = [
+    "light",
+    "dark",
+    "blue",
+    "pink",
+    "sunny",
+    "red",
+    "noisy",
+    "ugly",
+    "windy",
+    "furry",
+    "funny",
+    "sad",
+    "wet",
+    "broken",
+    "destroyed",
+    "lazy",
+    "happy",
+    "depressed",
 
-const B = []
+]
 
-const C = []
+const B = [
+    "grumpy",
+    "catastrophic",
+    "rainy",
+    "lucky",
+    "fluffy",
+    "fat",
+    "cute",
+    "random",
+    "lovely",
+    "gorilla",
+]
 
-const N = []
+const C = [
+    "hundred",
+    "million",
+    "arrow",
+    "heart",
+    "brain",
+    "plant",
+    "armadillion",
+    "side",
+    "ballon",
+    "seagull",
+    "boat",
+    "tree",
+    "ship",
+    "book",
+    "sailboat",
+    "flies",
+
+]
+
+const D = [
+    "zero",
+    "primary",
+    "supreme",
+    "the worst",
+]
 
 
 const generateRandomName = () => {
+    const firstWord = (A[getRandomInt(A.length-1)])
+    const secondWord = (B[getRandomInt(B.length-1)])
+    const thirdWord = (C[getRandomInt(C.length-1)])
+    const fourthWord = (D[getRandomInt(D.length-1)])
+    
+    return(firstWord +" "+ secondWord +" "+ thirdWord +" "+ fourthWord)
 
 }
 
-const randomName = generateRandomName();
+const randomName = generateRandomName(
+);
 
 console.log(randomName);
