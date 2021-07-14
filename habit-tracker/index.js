e52758fd91a8fdc0186975c120adb2f0817c2131
Alex Kupin <interstates21@gmail.com>
2021-07-14 20:38:48 +0300

    Created habits

---
+++ habit-tracker/index.js
@@ -0,0 +1,104 @@
+
+
+const HABITS = [
+    {
+        id: 'd7cbc56a-e4c8-11eb-ba80-0242ac130004',
+        name: 'Meditation',
+        priority: 5,
+        week: {
+            'Monday': false,
+            'Tuesday': false,
+            'Wednesday': false,
+            'Thursday': false,
+            'Friday': false,
+            'Saturday': false,
+            'Sunday': false,
+        }
+    },
+    {
+        id: 'ee78b930-e4c8-11eb-ba80-0242ac130004',
+        name: 'Guitar',
+        priority: 4,
+        week: {
+            'Monday': false,
+            'Tuesday': false,
+            'Wednesday': false,
+            'Thursday': false,
+            'Friday': false,
+            'Saturday': false,
+            'Sunday': false,
+        }
+    },
+    {
+        id: 'f7c02258-e4c8-11eb-ba80-0242ac130004',
+        name: 'Running',
+        priority: 5,
+        week: {
+            'Monday': false,
+            'Tuesday': false,
+            'Wednesday': false,
+            'Thursday': false,
+            'Friday': false,
+            'Saturday': false,
+            'Sunday': false,
+        }
+    },
+    {
+        id: '0b2da568-e4c9-11eb-ba80-0242ac130004',
+        name: 'Coding',
+        priority: 1,
+        week: {
+            'Monday': false,
+            'Tuesday': false,
+            'Wednesday': false,
+            'Thursday': false,
+            'Friday': false,
+            'Saturday': false,
+            'Sunday': false,
+        }
+    },
+    {
+        id: '05e2f2f2-e4c9-11eb-ba80-0242ac130004',
+        name: 'Reading business self-help books',
+        priority: 5,
+        week: {
+            'Monday': false,
+            'Tuesday': false,
+            'Wednesday': false,
+            'Thursday': false,
+            'Friday': false,
+            'Saturday': false,
+            'Sunday': false,
+        }
+    },
+]
+
+
+
+const findIndexById = (id) => {
+
+
+    return i;
+}
+
+const completeHabit = (habitId, dayName) => {
+}
+
+
+const renderHabit = () => {
+    
+}
+
+const renderHabits = () => {
+    let i = 0;
+}
+
+const displayStatsForHabit = () => {
+    // Calculate how many days completed for habit
+
+    // Render it
+}
+
+const createNewHabit = () => {
+    
+}
