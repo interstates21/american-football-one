6c47f1452f94259bc1f80c51d18b68f9065c8c2d
Alex Kupin <interstates21@gmail.com>
2021-07-24 12:01:06 +0300

    Timer

--- timer/index.js
+++ timer/index.js
@@ -1,10 +1,26 @@
-const button = document.getElementById("btn-start");
-let isFocused = false
-button.addEventListener("click", () => {
-  if (isFocused === false ){
-    isFocused = true
+let isFocusedInterval = false // if true, then we have rest
+let isTicking = false // if false, then it's paused
+
+const changePauseState = () => {
+  if (isTicking) {
+    button.innerHTML = 'Stop'
   }
   else {
-      isFocused = false
+    button.innerHTML = 'Start'
   }
+} 
+
+const button = document.getElementById("btn-start");
+button.addEventListener("click", () => {
+  isTicking = !isTicking;
+  changePauseState()
 })
+
+let seconds = 59;
+
+window.setInterval(() => {
+  const timeElement = document.getElementById('time');
+
+  timeElement.innerHTML = seconds;
+  seconds--;
+}, 500)
