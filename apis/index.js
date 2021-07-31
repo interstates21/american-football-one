703bb9a3d9f8bfba1c68b84162b477c6c015d13b
Alex Kupin <interstates21@gmail.com>
2021-07-31 10:35:50 +0300

    APIS

--- apis/index.js
+++ apis/index.js
@@ -1,14 +1,14 @@
 
 
 const getRandomDogPicture = () => {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("url")
     .then(response => {
+        console.log("response =", response)
 	    return response.json()
     })
     .then(data => {
-        console.log('data =', data)
-        const pictureElement = document.getElementById('picture-container')
-        pictureElement.style.background = `url("${data.message}") center / cover`
+        console.log("data =", data)
+        // Use data here
     })
     .catch(err => {
 	    console.error(err);
